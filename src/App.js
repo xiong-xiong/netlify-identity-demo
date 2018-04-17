946d9a0a565703c9017d275808742938f0c5d14a
Juha Stenroos <juha.stenroos@edita.fi>
2018-04-17 10:06:00 +0300

    added some fake content and css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,79 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import netlifyIdentity from "netlify-identity-widget"
+import netlifyIdentity from 'netlify-identity-widget';
 
+import Nav from "./components/Nav"
+import MainuserContent from "./components/MainuserContent"
+import UserContent from "./components/UserContent"
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      userName: "",
+      userRole: ""
+    }
+  }
+
+
   componentDidMount() {
     netlifyIdentity.init();
+  
   }
   handleIdentity = (e) => {
     e.preventDefault();
     netlifyIdentity.open();
+
   }
+
+  //This generates headers but they are not used for anything yet
+  generateHeaders() {
+    const headers = { "Content-Type": "application/json" };
+    if (netlifyIdentity.currentUser()) {
+      return netlifyIdentity.currentUser().jwt().then((token) => {
+        return { ...headers, Authorization: `Bearer ${token}` };
+      })
+    }
+    return Promise.resolve(headers);
+  } 
+
+
+
   render() {
+
+    //This is not secure way to check user role
+    netlifyIdentity.on("login", user => {
+      let userName = user.user_metadata.full_name;
+      let userRole = user.app_metadata.roles[0]
+      this.setState({
+        userName: userName,
+        userRole: userRole
+      })
+      if (userRole === "mainuser") {
+        console.log("You are mainuser")
+      } else {
+        console.log("You are not mainuser. You are " + this.state.userRole )
+      }
+    });
+
+    let content;
+    if(this.state.userRole === "mainuser") {
+     content = <div><MainuserContent /><UserContent /></div>
+    } else if(this.state.userRole === "user"){
+       content = <UserContent />
+    } else {
+      content = "You don't have rights to access this content"
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <p><a href="#" onClick={this.handleIdentity}>User Status</a></p>
-        </p>
+        <Nav userName={this.state.userName} handleIdentity={this.handleIdentity} />
+        <div className="container">
+          
+       
+            <p>{content}</p>
+          
+        </div>
       </div>
     );
   }
